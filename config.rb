d5eccc56ef4882b68c2b2a797371db932f16da8a
LightGuard <lightguard.jp@gmail.com>
2011-06-29 01:25:05 -0600

    Adding some style
    Using compass to build up the stylesheet, please make sure you have the
    compass gem installed and also the fancy-buttons gem installed if you
    want to regenerate the css

---
+++ config.rb
@@ -0,0 +1,29 @@
+require 'fancy-buttons'
+# Require any additional compass plugins here.
+
+
+# Set this to the root of your project when deployed:
+http_path = "/confbuzz"
+css_dir = "src/main/webapp/stylesheets"
+http_stylesheets_path = "/confbuzz/stylesheets"
+sass_dir = "src/main/sass"
+images_dir = "src/main/webapp/images"
+http_images_path = "/confbuzz/images"
+javascripts_dir = "src/main/webapp/javascripts"
+http_javascripts_path = "/confbuzz/javascripts"
+
+# You can select your preferred output style here (can be overridden via the command line):
+# output_style = :expanded or :nested or :compact or :compressed
+
+# To enable relative paths to assets via compass helper functions. Uncomment:
+# relative_assets = true
+
+# To disable debugging comments that display the original location of your selectors. Uncomment:
+# line_comments = false
+
+
+# If you prefer the indented syntax, you might want to regenerate this
+# project again passing --syntax sass, or you can uncomment this:
+# preferred_syntax = :sass
+# and then run:
+# sass-convert -R --from scss --to sass src/main/sass scss && rm -rf sass && mv scss sass
